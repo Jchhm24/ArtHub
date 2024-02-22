021b1982eca18d73abeb33fef61ea7e59b10476a
Julio Chan <Jcchm2004@gmail.com>
2024-02-22 01:45:50 -0600

    FiltradoCategorias

--- src/Components/Publicaciones/helpers/getImagenes.js
+++ src/Components/Publicaciones/helpers/getImagenes.js
@@ -2,7 +2,7 @@ export const getImagenes = async (ids) => {
     const response = await fetch('https://www.arthub.somee.com/api/Publicacion/')
     let data = await response.json()
 
-    // Filtra las imágenes basándote en las ids
+    // Filtra las imágenes basándote en las ids(categories) seleccionadas de los checkboxes
     if (ids && ids.length > 0) {
         data = data.filter(imagen => {
             const categorias = imagen.categoria.split(',');

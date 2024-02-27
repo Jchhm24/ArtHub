d9f46575f965e43e03754c94374aad8c4664732d
Julio Chan <Jcchm2004@gmail.com>
2024-02-27 12:17:05 -0600

    seguridad

--- src/Components/Publicaciones/helpers/getImagenes.js
+++ src/Components/Publicaciones/helpers/getImagenes.js
@@ -1,25 +1,29 @@
 export const getImagenes = async (ids, searchTerm) => {
-    const response = await fetch('https://www.arthub.somee.com/api/Publicacion/')
-    let data = await response.json()
+    try {
+        const response = await fetch('https://www.arthub.somee.com/api/Publicacion/')
+        let data = await response.json()
 
-    // Filtra las imágenes basándote en las ids(categories) seleccionadas de los checkboxes
-    if (ids && ids.length > 0) {
-        data = data.filter(imagen => {
-            const categorias = imagen.categoria.split(',');
-            return categorias.some(categoria => ids.includes(categoria));
-        });
+        // Filtra las imágenes basándote en las ids(categories) seleccionadas de los checkboxes
+        if (ids && ids.length > 0) {
+                data = data.filter(imagen => {
+                        const categorias = imagen.categoria.split(',');
+                        return categorias.some(categoria => ids.includes(categoria));
+                });
+        }
+
+        // Buscar las imagenes por medio de sus titulos
+        if (searchTerm) {
+                data = data.filter(imagen => imagen.titulo.toLowerCase().includes(searchTerm.toLowerCase()));
+        }
+
+        // Desordena las imagenes para dar el efecto random
+        for (let i = data.length - 1; i > 0; i--) {
+                const j = Math.floor(Math.random() * (i + 1));
+                [data[i], data[j]] = [data[j], data[i]];
+        }
+
+        return data
+    } catch (error) {
+        console.error('Ocurrió un error al obtener las imágenes:', error);
     }
-
-    // Buscar las imagenes por medio de sus titulos
-    if (searchTerm) {
-        data = data.filter(imagen => imagen.titulo.toLowerCase().includes(searchTerm.toLowerCase()));
-    }
-
-    // Desordena las imagenes para dar el efecto random
-    for (let i = data.length - 1; i > 0; i--) {
-        const j = Math.floor(Math.random() * (i + 1));
-        [data[i], data[j]] = [data[j], data[i]];
-    }
-
-    return data
 }

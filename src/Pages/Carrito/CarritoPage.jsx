5b9977796a0a1d853525a5c441660e2ffb126705
Julio Chan <Jcchm2004@gmail.com>
2024-03-12 10:18:21 -0600

    Delete-inutilCode

--- src/Pages/Carrito/CarritoPage.jsx
+++ src/Pages/Carrito/CarritoPage.jsx
@@ -1,10 +1,10 @@
-import { useEffect, useState } from "react"
+import { useState } from "react"
 import { CarritoOptions } from "./Components/CarritoOptions/CarritoOptions"
 
 export const CarritoPage = () => {
 
     // Aqui almacenamos el carrito
-    const [carrito, setCarrito] = useState(JSON.parse(localStorage.getItem('carrito') || []))
+    const [carrito, setCarrito] = useState(JSON.parse(localStorage.getItem('carrito')))
     
     const userId = JSON.parse(localStorage.getItem('userData')).idUsuario
 
@@ -14,11 +14,6 @@ export const CarritoPage = () => {
       // Ahora actualizamos el localStorage del carrito, le tenemos que pasar al carrito actualizado
       localStorage.setItem('carrito', JSON.stringify(newCarrito))
     }
-
-    // Usamos useEffect para imprimir carrito cada vez que se actualiza además de que se actualiza el carrito en localStorage
-    useEffect(() => {
-      console.table(carrito)
-    }, [carrito])
     
     return (
       <>

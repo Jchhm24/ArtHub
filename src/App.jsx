b18e159d1731cc82a727674fc406f08db5623666
Julio Chan <Jcchm2004@gmail.com>
2024-03-21 08:20:22 -0600

    Cambio de variable para los perfiles

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import { PreviewPage } from "./Pages/PublicacionPreview/PreviewPage";
 import { PerfilPage } from "./Pages/Perfil/PerfilPage";
 import { CarritoPage } from "./Pages/Carrito/CarritoPage";
 import { Dashboardpage } from "./Pages/Dashboard/Dashboardpage";
+import { PerfilUserPage } from "./Pages/PerfilUser/PerfilUserPage";
 
 export const App = () => {
   const [activeCheckboxes, setActiveCheckboxes] = useState([]);
@@ -23,22 +24,25 @@ export const App = () => {
 
   const getImgId = (id) => {
     setIdPublicacion(id)
+    setUserId(id)
   }
 
-  // Si esta en true no manda al inicio de la pagina pero si esta en false nos manda al perfil
-  const [statePage, setStatePage] = useState(true)
+  // Si esta en 1 nos manda al inicio de la pagina, si es en 2 nos manda al perfi y si es 3 nos manda al perfil de otros usuarios
+  const [statePage, setStatePage] = useState(1)
+
+  const [userId, setUserId] = useState(0)
 
   return (
     <>
       <NavComponent searchTerm={searchTerm} setSearchTerm={setSearchTerm} changePage={changePage} setStatePage={setStatePage}/>
- 
       {
         // Accesos directos del nav
-        page === 'inicio' && <InicioPage activeCheckboxes={activeCheckboxes} setActiveCheckboxes={setActiveCheckboxes} searchTerm={searchTerm} changePage={changePage} getImgId={getImgId} setStatePage={setStatePage}/>
+        page === 'inicio' && <InicioPage activeCheckboxes={activeCheckboxes} setActiveCheckboxes={setActiveCheckboxes} searchTerm={searchTerm} changePage={changePage} getImgId={getImgId}/>
         || page === 'carrito' && <CarritoPage/>
         || page ===  'publicar' && <PublicarPage changePage={changePage}/>
         // Vista de la publicacion
-        || page === 'preview' && <PreviewPage id={idPublicacion} changePage={changePage} statePage={statePage}/>
+        || page === 'preview' && <PreviewPage id={idPublicacion} changePage={changePage} statePage={statePage} setUserId={setUserId}/>
+        || page === 'perfilUser' && <PerfilUserPage userId={userId} changePage={changePage} getImgId={getImgId} setStatePage={setStatePage} />
         //Opciones del dropwdown del perfil
         || page === 'perfil' && <PerfilPage changePage={changePage} getImgId={getImgId} setStatePage={setStatePage}/>
         || page === 'dashboard' && <Dashboardpage changePage={changePage}/>

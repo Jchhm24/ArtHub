9df91cdc3bfc8ad66ca997a0d6c88d3ca378d276
Jchhm24 <jcchm2004@gmail.com>
2024-04-08 12:24:44 -0600

    Cambio de formData

--- src/Pages/Perfil/EditarPerfil/helpers/putUser.js
+++ src/Pages/Perfil/EditarPerfil/helpers/putUser.js
@@ -1,24 +1,37 @@
 import { getUpdateUser } from "./getUser";
 
-export const putUser = async (id, updateUser) => {
-    fetch(`https://arthub.somee.com/api/Registro_Usuario/${id}`, {
-    method: 'PUT',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify(updateUser)
-  })
-  .then(res => {
-    if(res.ok){
-        alert('Tus datos han sido actualizados')
-        // TODO:Configurar esto para actualizar los datos del usuario
-        localStorage.removeItem('userData');
-        getUpdateUser(id)
-    }else{
-        alert('Error al actualizar los datos')
+export const putUser = async (id, formState, user, confirmModal) => {
+
+  const {fotoPerfil, nombre, apellido, username, email, contrasena, idRol} = formState
+  const updateUser = new FormData()
+  // le añadimos los datos que desestructuramos del formState
+  if(user.username !== username || user.email !== email || user.contrasena !== contrasena || user.idRol !== idRol){
+    // TODO: Mandar con la contraseña original
+    confirmModal(true)
+    // !pongo el return para que no se ejecute el codigo de envio de datos
+    return
+  }else{
+    updateUser.append('FotoPerfil', fotoPerfil)
+    updateUser.append('Nombre', nombre)
+    updateUser.append('Apellido', apellido)
+  }
+
+  try{
+    const response = await fetch(`https://arthub.somee.com/api/Registro/${id}`,{
+      method: 'PATCH',
+      body: updateUser
+    })
+
+    // Validar si todo esta bien
+    if(!response.ok){
+      throw new Error('Hubo un error al actualizar el usuario')
     }
-  })
-  .catch(error =>{
-    console.log('Error al actualizar usuario', error)
-  })
+
+    // Si el codigo de aqui abajo se ejecuta quiere decir qeu todo salio chido bb
+    alert('Usuario actualizado correctamente')
+    getUpdateUser(id)
+  }catch(error){
+    alert('Hubo un error al actualizar el usuario')
+    console.log('Errores ocuridos:', error)
+  }
 }

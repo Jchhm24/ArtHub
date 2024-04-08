24b4af995c9731ed1daecc8288659eeebb10405a
Jchhm24 <jcchm2004@gmail.com>
2024-04-08 12:24:22 -0600

    Cambio de URL

--- src/Pages/Perfil/EditarPerfil/helpers/getUser.js
+++ src/Pages/Perfil/EditarPerfil/helpers/getUser.js
@@ -1,5 +1,5 @@
 export const getUpdateUser = async (id) => {
-    const response = await fetch(`https://arthub.somee.com/api/Registro_Usuario/${id}`)
+    const response = await fetch(`https://arthub.somee.com/api/Registro/${id}`)
     const DataUpdate = await response.json()
 
     localStorage.setItem('userData', JSON.stringify(DataUpdate))
